36ca065fc0e42c34efb6c493f2b9cbbeca954f37
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-02 01:13:14 +0300

    ask 25 added

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число A: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int pow = int.Parse(Console.ReadLine());
+int GetPow(int a, int b)
+{
+    int i = 0;
+    int prod = 1;
+    while ( i < pow)
+{
+        if (a ==0 && b == 0) return 1;
+        prod = number*prod;
+        i++;
+    
+}
+return prod;
+
+}
+
+Console.WriteLine(GetPow(number, pow));
