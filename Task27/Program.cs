3c8928233547f85cc33da7ccf9c59d5542e85a81
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-02 01:39:53 +0300

    Task27 completed

---
+++ Task27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int getDigit(int num)
+{
+    if (num == 0) return 1;
+    int count = 0;
+    for (int i = 0; num != 0; i++)
+        {
+            num = num/10;
+            count+=1; 
+        }
+        return count;    
+}
+Console.WriteLine(getDigit(number));
