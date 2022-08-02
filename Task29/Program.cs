5baad40f70f23bd043d4317a9fab51d28fb895bc
svetlana matsyevskaya <svetlanamats@mail.ru>
2022-08-02 12:19:46 +0300

    Task29 completed

---
+++ Task29/Program.cs
@@ -0,0 +1,24 @@
+﻿
+void FillArray(int[] matr)
+{
+  for (int i = 0; i < matr.Length; i++)
+  {
+    matr[i] = new Random().Next(1,100);
+  }
+    
+}
+void PrintArray(int[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        Console.Write($"{matrix[i]} ");
+    }
+}
+int [] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int [] array2 = {2,4,6,8,9,0,4,9};
+PrintArray(array2);
